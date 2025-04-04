2fcd2522efcefa8240184b530b36f79606aa452b
Armando <zavalaa4@msu.edu>
2025-04-03 20:56:02 -0400

    Added new elements
    Added elements stone, dirt, fire, wood, and steam. Added new features like if water touches dirt it turns into grass, if fire touches either wood or wet wood it burns it and creates more fire, although wet wood takes more time to burn. Also, when fire touches water it turns into steam. When wet wood gets into contact with normal wood it changes it into wet wood and to get wet wood it has to touch water.

--- particles.js
+++ particles.js
@@ -69,14 +69,20 @@ export class Sand extends Particle {
 export function checkParticleType(value) {
     if (value == "Sand") {
         return new Sand();
-    } 
-    // TODO create new particles
-    if (value == "Sand") {
-        return new Sand();
-    }
-    if (value == "Water") {
+    } else if (value == "Water") {
         return new Water();
+    } else if (value == "Stone") {
+        return new Stone();
+    } else if (value == "Dirt") {
+        return new Dirt();
+    } else if (value == "Fire") {
+        return new Fire();
+    } else if (value == "Wood") {
+        return new Wood();
+    } else if (value == "Steam") {
+        return new Steam();
     }
+    return null;
 }
 
 export class Water extends Particle {
@@ -87,6 +93,19 @@ export class Water extends Particle {
     }
 
     update(row, col) {
+        // Make water turn dirt into grass when it touches it
+        if (getParticle(row+1, col)?.type == "dirt") {
+            // Remove water and change dirt to grass
+            setParticle(row+1, col, new Grass());
+            setParticle(row, col, null);
+            return;
+        } 
+
+        if (getParticle(row+1, col)?.type == "wood") {
+            setParticle(row+1, col, new WetWood());
+            setParticle(row, col, null);
+            return;
+        }
         // Try to move down
         if (getRandomInt(0, 2) && !getParticle(row+1, col)) {
             moveParticle(row, col, row+1, col, super.swap);
@@ -98,6 +117,226 @@ export class Water extends Particle {
         }
         else if (!getParticle(row, col-1)) {
             moveParticle(row, col, row, col-1, super.swap);
+        }
+    }
+}
+
+export class Stone extends Particle {
+    constructor () {
+        super();
+        this.color = "gray";
+        this.type = "stone";
+    }
+}
+
+export class Dirt extends Sand {
+    constructor() {
+        super();
+        this.color = "brown";
+        this.type = "dirt";
+    }
+}
+
+export class Grass extends Sand {
+    constructor() {
+        super();
+        this.color = "green";
+        this.type = "grass";
+    }
+}
+
+export class Fire extends Particle {
+    constructor() {
+        super();
+        this.color = "yellow";
+        this.type = "fire";
+        this.maxDuration = 25;
+        this.duration = 0;
+    }
+
+    update(row, col) {
+        // Update duration
+        this.duration++;
+
+        const life = this.duration / this.maxDuration;
+        if (life < 0.3) {
+            this.color = "rgb(255, 200, 0)";
+        } else if (life < 0.6) {
+            this.color = "rgb(255, 100, 0)";
+        } else {
+            this.color = "rgb(200, 0, 0)";
+        }
+
+        // Burns wood
+        if (getParticle(row+1, col)?.type == "wood") {
+            // Remove wood and change to fire
+            setParticle(row+1, col, new Fire());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row-1, col)?.type == "wood") {
+            setParticle(row-1, col, new Fire());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row, col+1)?.type == "wood") {
+            setParticle(row, col+1, new Fire());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row, col-1)?.type == "wood") {
+            setParticle(row, col-1, new Fire());
+            setParticle(row, col, null);
+            return;
+        }
+
+        // Turns water into steam
+        if (getParticle(row+1, col)?.type == "water") {
+            // Remove water and turns into steam
+            setParticle(row+1, col, new Steam());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row-1, col)?.type == "water") {
+            setParticle(row-1, col, new Steam());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row, col+1)?.type == "water") {
+            setParticle(row, col+1, new Steam());
+            setParticle(row, col, null);
+            return;
+        } else if (getParticle(row, col-1)?.type == "water") {
+            setParticle(row, col-1, new Steam());
+            setParticle(row, col, null);
+            return;
+        }
+
+        // Burns wet wood
+        if (getParticle(row+1, col)?.type == "wet wood") {
+            // Remove wet wood and turn to fire
+            if (getRandomInt(0, 100) < 5) {
+                setParticle(row+1, col, new Fire());
+                setParticle(row, col, null);
+                return;
+            }
+        } else if (getParticle(row-1, col)?.type == "wet wood") {
+            if (getRandomInt(0, 100) < 5) {
+                setParticle(row-1, col, new Fire());
+                setParticle(row, col, null);
+                return;
+            }
+        }   else if (getParticle(row, col+1)?.type == "wet wood") {
+            if (getRandomInt(0, 100) < 5) {
+                setParticle(row, col+1, new Fire());
+                setParticle(row, col, null);
+                return;
+            }
+        } else if (getParticle(row, col-1)?.type == "wet wood") {
+            if (getRandomInt(0, 100) < 5) {
+                setParticle(row, col-1, new Fire());
+                setParticle(row, col, null);
+                return;
+            }
+        }
+
+        // Try to move up
+        if (getRandomInt(0, 2) && !getParticle(row-1, col)) {
+            moveParticle(row, col, row-1, col, super.swap);
+        } 
+        
+        // Move left or right
+        if (getRandomInt(0, 1) && !getParticle(row, col+1)) {
+            moveParticle(row, col, row, col+1, super.swap);
+        }
+        else if (!getParticle(row, col-1)) {
+            moveParticle(row, col, row, col-1, super.swap);
+        }
+
+        // Check fire duration
+        if (this.duration >= this.maxDuration) {
+            setParticle(row, col, null);
+        }
+    }
+}
+
+export class Wood extends Stone {
+    constructor() {
+        super();
+        this.color = "rgb(150, 75, 0)";
+        this.type = "wood";
+    }
+}
+
+export class WetWood extends Wood {
+    constructor() {
+        super();
+        this.color = "rgb(157, 108, 60)";
+        this.type = "wet wood";
+        this.spreadTime = 5
+    }
+
+    update(row, col) {
+        // Turn wood into wet wood
+        if (getParticle(row+1, col)?.type == "wood") {
+            if (getRandomInt(0, 100) < this.spreadTime) {
+                setParticle(row+1, col, new WetWood());
+                return;
+            }
+        } else if (getParticle(row-1, col)?.type == "wood") {
+            if (getRandomInt(0, 100) < this.spreadTime) {
+                setParticle(row-1, col, new WetWood());
+                return;
+            }
+        } else if (getParticle(row, col+1)?.type == "wood") {
+            if (getRandomInt(0, 100) < this.spreadTime) {
+                setParticle(row, col+1, new WetWood());
+                return;
+            }
+        } else if (getParticle(row, col-1)?.type == "wood") {
+            if (getRandomInt(0, 100) < this.spreadTime) {
+                setParticle(row, col-1, new WetWood());
+                return;
+            }
+        }
+    }
+}
+
+export class Steam extends Particle {
+    constructor() {
+        super();
+        this.color = "rgb(240, 240, 240)";
+        this.type = "steam";
+        this.maxDuration = 200;
+        this.duration = 0;
+    }
+
+    update(row, col) {
+        this.duration++;
+
+        const life = this.duration / this.maxDuration;
+        if (life < 0.3 ) {
+            this.color = "rgb(230, 230, 235)"
+        } else if (life < 0.6) {
+            this.color = "rgb(220, 220, 225)"
+        } else {
+            this.color = "rgb(190, 200, 210)"
+        }
+        // Moves up
+        if (getRandomInt(0, 2) && !getParticle(row-1, col)) {
+            moveParticle(row, col, row-1, col, super.swap);
+        } 
+        
+        // Move left or right
+        if (getRandomInt(0, 1) && !getParticle(row, col+1)) {
+            moveParticle(row, col, row, col+1, super.swap);
+        }
+        else if (!getParticle(row, col-1)) {
+            moveParticle(row, col, row, col-1, super.swap);
+        }
+
+        // Check steam duration
+        if (this.duration >= this.maxDuration) {
+            setParticle(row, col, null);
+        }
+
+        if (getRandomInt(0, 1000) < 1) {
+            setParticle(row, col, new Water());
         }
     }
 }
